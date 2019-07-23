9a78ea9023791a9c05ebc22a7746d3a3e6865d41
sheggen <heggens@berea.edu>
2019-07-23 11:07:10 -0400

    Adding webserver with livefeed

--- webServer/app/controllers/main_controllers.py
+++ webServer/app/controllers/main_controllers.py
@@ -1,17 +1,29 @@
 # from flask import render_template  #, redirect, url_for, request, g, current_app
 # from flask_login import current_user, login_required
-from flask import jsonify, render_template, request, redirect, url_for
+from flask import jsonify, render_template, request, redirect, url_for, Response
 from app.controllers import main_bp
 # from app.controllers.liveCamera import *
 from app import app, allowed_file, cfg
 from werkzeug.utils import secure_filename
-import os
+import os, cv2, socket, io
+
+vc = cv2.VideoCapture(0)
 
 @main_bp.route('/index', methods=['GET'])
 def index_GET():
     images = os.listdir(cfg["upload_folder"])
-
     return render_template("index.html", images = images)
+
+def videoStream():
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite("pic.jpg", frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
+@main_bp.route('/video_feed')
+def video_feed():
+    return Response(videoStream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @main_bp.route("/addImage", methods=["POST"])
 def changeImage():
@@ -31,7 +43,7 @@ def changeImage():
         file.save(os.path.join(cfg["upload_folder"], filename))
 
         return jsonify({"success": True,
-                        "filename": file.filename})
+                        "filename": filename})
 
 @main_bp.route("/switchActiveImage/<filename>", methods=["POST"])
 def switchActiveImage(filename):
